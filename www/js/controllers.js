1fa40fe3663badd685fb74fbf2fcb33807ebf7d4
Gareth Shaughnessy <garethshaughnessy@garethsessysmbp.home>
2015-04-09 17:26:42 +0100

    changed controllers to feed the food data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,73 @@
 angular.module('calCounter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
+
+.controller('AppCtrl', function($scope) {
+ $scope.categories = ['Meat', 'Vegetable', 'Fruit', 'Fish & Poultry' , 'Drinks'];
   
 })
 
 .controller('FoodlistsCtrl', function($scope, $stateParams) {
+    categories = ['Meat', 'Vegetable', 'Fruit', 'Fish&Poultry' , 'Drinks'];
     
-    $scope.dishesId = $stateParams.dishesId;
+    foodlists = [
+    [
+      "back bacon",
+      "beef fillet",
+      "pork sausage",
+      "pork chop",
+       "pork fillet",
+      "roast beef",
+      "roast lamb",
+      "sirlion steak",
+       "streaky bacon"
+    ],
+    [
+      "asparagus",
+      "broccoli",
+      "onion",
+      "cabbage",
+      "carrot",
+      "courgette",
+      "greenbeans",
+      "parsnip",
+      "red pepper",
+       "tomato",
+    ],
+    [
+      "apple",
+      "banana",
+      "blueberries",
+      "kiwi",
+      "orange",
+      "peach",
+      "pear",
+      "pinepple",
+      "strawberry",
+
+    ],
+    [
+      "chicken breast",
+      "duck breast",
+      "lobster",
+     "prawn",
+      "salmon",
+      "swordfish",
+      "tuna",
+      "turkey",
+         ],
+    [
+        
+      "fizzy drink",
+      "champagne",
+      "cider",
+      "beer",
+      "stout",
+      "white wine",
+      "red wine",
+     
+    ]
+ ];
+    
+ $scope.category = categories[$stateParams.foodlistsId];
+    $scope.foodlists = foodlists[$stateParams.foodlistsId];
 });
